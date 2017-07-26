88318d3abe61d189c9cf544cc68912389bfcfb55
Cipriano Sanchez <ciprianosanchez@gmail.com>
2017-07-26 18:48:56 +0200

    initial commit

---
+++ src/main/java/com/dogoauth/DogoauthApplication.java
@@ -0,0 +1,12 @@
+package com.dogoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogoauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogoauthApplication.class, args);
+	}
+}
