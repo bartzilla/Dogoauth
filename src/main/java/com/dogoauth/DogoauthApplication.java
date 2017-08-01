e0d563b43a1ba62fc99ddb05f6f04ca16144cd9e
Cipriano Sanchez <ciprianosanchez@gmail.com>
2017-08-01 10:55:52 +0200

    Basic client credentials grant type configuration

--- src/main/java/com/dogoauth/DogoauthApplication.java
+++ src/main/java/com/dogoauth/DogoauthApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DogoauthApplication {
+public class DogoauthApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+    {
 		SpringApplication.run(DogoauthApplication.class, args);
 	}
 }
